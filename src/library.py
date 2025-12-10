defa3a0ddf6ad10c000411265cffe90d2554589f
AlRoman747 <romaaltahir@icloud.com>
2025-12-10 22:25:12 +0300

    add dict-collection

--- src/library.py
+++ src/library.py
@@ -14,12 +14,14 @@ class BookCollection:
     def add(self, book: Book):
         self._items.append(book)
 
+    def remove(self, book: Book):
+        self._items.remove(book)
+
     def __getitem__(self, key):
         if isinstance(key, int):
             return self._items[key]
 
         if isinstance(key, slice):
-            # срез → возвращаем *новый BookList* на тех же объектах
             new_list = BookCollection()
             for item in self._items[key]:
                 new_list.add(item)
@@ -32,3 +34,23 @@ class BookCollection:
         return len(self._items)
 
 
+
+class BookCatalog:
+    def __init__(self):
+        self._by_isbn: dict[str, Book] = {}  # словарь по isbn
+        self._by_author: dict[str, list[Book]] = {} # словарь по автору
+        self._by_year: dict[int, list[Book]] = {} # словарь по году
+
+    def add(self, book: Book):
+        self._by_isbn[book.isbn] = book
+        self._by_year.setdefault(book.year, []).append(book)
+        self._by_author.setdefault(book.author, []).append(book)
+
+    def get_by_isbn(self, isbn: str) -> Book:
+        return self._by_isbn.get(isbn)
+
+    def get_by_author(self, author: str) -> list:
+        return self._by_author.get(author, [])
+
+    def get_by_year(self, year: int) -> list:
+        return self._by_year.get(year, [])

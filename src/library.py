1e269e25f480ae3e6cebc492dafc66c52877fe03
AlRoman747 <romaaltahir@icloud.com>
2025-12-14 12:27:04 +0300

    fixed types in dict keys and add random simulation

--- src/library.py
+++ src/library.py
@@ -1,3 +1,6 @@
+from logging import exception
+
+
 class Book:
     # книга, все данные о книге
     def __init__(self, title: str, author: str, year: int, genre: str, isbn: str):
@@ -8,9 +11,15 @@ class Book:
         self.isbn = isbn
 
 
+    def __repr__(self):
+        return f'{self.title} {self.author} {self.year} {self.genre} {self.isbn}'
+
+
+
+
 class BookCollection:
-    def __int__(self):
-        self._items: list[Book] = []
+    def __init__(self):
+        self._items = []
 
     def add(self, book: Book):
         self._items.append(book)
@@ -19,56 +28,55 @@ class BookCollection:
         self._items.remove(book)
 
     def __getitem__(self, key):
-        if isinstance(key, int):
-            return self._items[key]
+        try:
+            if isinstance(key, int):
+                return self._items[key]
 
-        if isinstance(key, slice):
-            new_list = BookCollection()
-            for item in self._items[key]:
-                new_list.add(item)
-            return new_list
-
-    def __iter__(self):
+            if isinstance(key, slice):
+                new_list = BookCollection()
+                for item in self._items[key]:
+                    new_list.add(item)
+                return new_list
+        except: raise IndexError("такой книги нет")
+    def __iter__(self) -> list:
         return iter(self._items)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self._items)
 
 
 
 class BookCatalog:
     def __init__(self):
-        self._by_isbn: dict[str, Book] = {}  # словарь по isbn
-        self._by_author: dict[str, list[Book]] = {} # словарь по автору
-        self._by_year: dict[int, list[Book]] = {} # словарь по году
+        self._by_isbn = {}  # словарь по isbn
+        self._by_author = {} # словарь по автору
+        self._by_year = {} # словарь по году
 
     def add(self, book: Book):
-        self._by_isbn[book.isbn] = book
-        self._by_year.setdefault(book.year, []).append(book)
-        self._by_author.setdefault(book.author, []).append(book)
+        self._by_isbn[str(book.isbn)] = book
+        self._by_year.setdefault(int(book.year), []).append(book)
+        self._by_author.setdefault(str(book.author) , []).append(book)
 
     def remove(self, book: Book):
         # будем удалять, только если у книги правильный isbn, в других случаях даже не проверяем всё остальное
-        if book.isbn in self._by_isbn.keys():
+        try:
             del_book = self._by_isbn[book.isbn]
             del self._by_isbn[book.isbn]
-            if book.year in self._by_year.keys(): self._by_year[book.year].remove(del_book)
-            else: raise ValueError("Указан неправильный год книги для удаления")
-            if book.author in self._by_author.keys(): self._by_author[book.author].remove(del_book)
-            else: raise ValueError("Указан неправильный автор книги для удаления")
-        else: raise ValueError("Указан неправильный isbn книги для удаления")
+            self._by_year[book.year].remove(del_book)
+            self._by_author[book.author].remove(del_book)
+        except: raise ValueError("Указан неправильный isbn книги для удаления")
     def get_by_isbn(self, isbn: str) -> Book:
-        if isbn in self._by_isbn.keys(): return self._by_isbn.get(isbn)
-        raise ValueError("Указан неправильный isbn книги")
+        try: return self._by_isbn[str(isbn)]
+        except: raise KeyError("Указан неправильный isbn книги")
 
     def get_by_author(self, author: str) -> list:
-        if author in self._by_author.keys(): return self._by_author.get(author, [])
-        raise ValueError("Указан неправильный автор книги")
+        try: return self._by_author[str(author)]
+        except: raise KeyError("Указан неправильный автор книги")
 
 
     def get_by_year(self, year: int) -> list:
-        if year in self._by_year.keys(): return self._by_year.get(year, [])
-        raise ValueError("Указан неправильный год книги")
+        try: return self._by_year[int(year)]
+        except: raise KeyError("Указан неправильный год книги")
 
 
 
@@ -84,5 +92,5 @@ class Library:
     def search_by_year(self, year: int) -> list:
         return self.IndexDict.get_by_year(year)
 
-
+    
 
